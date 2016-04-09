cae289c4affbd027a9e6e2790f815456b77f1d9f
beth <zhangbaige@innobuddy.com>
2016-04-09 13:06:48 +0800

    添加react组件测试

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -3,3 +3,4 @@ echo ""
 ./node_modules/.bin/babel --plugins "transform-runtime" src --ignore __tests__ --out-dir ./dist
 echo ""
 echo "> Complete transpiling ES2015"
+
--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,52 @@
-import {expect} from 'chai';
-import {sum} from '../';
-const {describe, it} = global;
+import React from 'react';
 
-describe('sum', () => {
-  it('should add two numbers correctly', async () => {
-    const result = await sum(10, 20);
-    expect(result).to.be.equal(30);
-  });
+import {shallow, mount, render} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+import MyComponent from '../index';
+
+describe('Shallow Rendering', () => {
+
+    it('to have three `.icon-test`s', () => {
+        const wrapper = shallow(<MyComponent />);
+        expect(wrapper.find('.icon-test')).to.have.length(3);
+    });
+
+    it('simulates click events', () => {
+        const buttonClick = sinon.spy();
+        const wrapper = shallow(
+            <MyComponent handleClick={buttonClick} />
+        );
+        wrapper.find('button').simulate('click');
+        expect(buttonClick.calledOnce).to.equal(true);
+    });
+
+});
+
+describe('Full DOM Rendering', () => {
+
+    it('allows us to set props', () => {
+        const wrapper = mount(<MyComponent bar='baz' />);
+        expect(wrapper.props().bar).to.equal('baz');
+        wrapper.setProps({ bar: 'foo' });
+        expect(wrapper.props().bar).to.equal('foo');
+    });
+
+    it('calls componentDidMount', () => {
+        sinon.spy(MyComponent.prototype, 'componentDidMount');
+        const wrapper = mount(<MyComponent />);
+        expect(MyComponent.prototype.componentDidMount.calledOnce).to.be.true;
+        MyComponent.prototype.componentDidMount.restore();
+    });
+
+});
+
+describe('Static Rendered Markup', () => {
+
+    it('renders three `.icon-test`s', () => {
+        const wrapper = render(<MyComponent />);
+        expect(wrapper.find('.icon-test').length).to.equal(3);
+    });
+
 });
